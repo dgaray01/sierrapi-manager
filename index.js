255424d0b278fd890aa08bc60b27ffb71b0af32a
Daniel Garay <50188641+iStyLEX23@users.noreply.github.com>
2022-02-08 06:09:44 -0800

    Installed packages

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ Makesa website that we can access privately or public
 depending on your router settings, this is a website
 that manages your SierraPI configuration.
 */
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
