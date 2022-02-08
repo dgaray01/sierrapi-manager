59be91d0094455edd3a06b5e37d7097b97b955d8
Daniel Garay <50188641+iStyLEX23@users.noreply.github.com>
2022-02-08 05:13:30 -0800

    Create index.js and basic info

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+SierraPI Server-Manager
+
+Makesa website that we can access privately or public 
+depending on your router settings, this is a website
+that manages your SierraPI configuration.
+*/
