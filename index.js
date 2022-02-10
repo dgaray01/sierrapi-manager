35dbdad7c3d90a81bcd2fa566a7f0bc43e4db1ed
Daniel Garay <50188641+iStyLEX23@users.noreply.github.com>
2022-02-10 14:59:16 -0800

    Added Website & Index modifications.

--- index.js
+++ index.js
@@ -7,21 +7,34 @@ that manages your SierraPI configuration.
 */
 var express = require('express');
 var app = express();
+const mailgun = require("mailgun-js");
+const DOMAIN = 'YOUR_DOMAIN_NAME';
+//const mg = mailgun({apiKey: api_key, domain: DOMAIN});
 
-// set the view engine to ejs
+
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
-
-// index page
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('index');
 });
 
-// about page
+
 app.get('/about', function(req, res) {
   res.render('pages/about');
 });
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
+
+/*
+const data = {
+	from: 'Excited User <me@samples.mailgun.org>',
+	to: 'bar@example.com, YOU@YOUR_DOMAIN_NAME',
+	subject: 'Hello',
+	text: 'Testing some Mailgun awesomness!'
+};
+mg.messages().send(data, function (error, body) {
+	console.log(body);
+});
+*/
+
